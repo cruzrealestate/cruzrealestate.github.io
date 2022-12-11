6f3d62a619191d82c472131489d798d14cf365d8
LC <bigswole19@yahoo.com>
2022-12-11 18:24:56 -0500

    --checkpoint--

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@
 //
 // Scripts
 // 
+var debug;
 
 function Translate() {
   //initialization
@@ -23,6 +24,7 @@ function Translate() {
       if (xrhFile.readyState === 4) {
         if (xrhFile.status === 200 || xrhFile.status == 0) {
           var LngObject = JSON.parse(xrhFile.responseText);
+          debug = LngObject
           var allDom = document.getElementsByTagName("*");
           for (var i = 0; i < allDom.length; i++) {
             var elem = allDom[i];
